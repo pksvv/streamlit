fa60c69e995c956c2b9fb75715918a3e4a5fb6a4
gaurvipul@gmail.com <gaurvipul@gmail.com>
2022-01-02 06:47:45 +0530

    streamlit-data-basics

--- data.py
+++ data.py
@@ -1,3 +1,13 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+
+a = list(range(1,10))
+
+#l = a.res
+
+st.write(type(a),a)
+
+df = pd.DataFrame(np.array(a).reshape((3,3)),columns='C1 C2 C3'.split())
+
+st.write(df)
