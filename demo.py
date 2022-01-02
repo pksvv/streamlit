2d0d389221abd2e20d413daf051422584a37c8b2
gaurvipul@gmail.com <gaurvipul@gmail.com>
2022-01-02 06:20:22 +0530

    streamlit-basics

---
+++ demo.py
@@ -0,0 +1,14 @@
+import streamlit as st
+
+st.title("Hello World!")
+st.header("Example Header")
+st.subheader("Example Sub-Header")
+st.text("This is an example text.")
+
+st.markdown(""" # h1 tag
+## h2 tag
+### h3 tag 
+:moon: <br>
+:sunglasses:""",True)
+
+st.latex(r"""2x + \frac{2}{3}x^{\frac{3}{2}} + \frac{1}{x}\Biggr|_{1}^{9}""")
