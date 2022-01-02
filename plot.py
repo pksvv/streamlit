c5971aa9e548b3a463ac77f65268b1a45fe7ff8e
gaurvipul@gmail.com <gaurvipul@gmail.com>
2022-01-02 08:40:53 +0530

    streamlit-plotting

--- plot.py
+++ plot.py
@@ -1,5 +1,20 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
 
 df = pd.DataFrame(np.random.randn(100,3),columns='A B C'.split())
+
+fig, ax = plt.subplots()
+
+ax.scatter(df['A'],df['B'])
+st.pyplot(fig)
+
+plt.scatter(df['A'],df['B'])
+plt.title('Scatter Plot')
+st.pyplot();
+
+st.line_chart(df)
+st.area_chart(df)
+st.bar_chart(df)
+st.write(df)
